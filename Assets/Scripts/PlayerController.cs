030445f0835f1553eebd1e8e777c75089cb0c7e2
Robert Johnson <robertjo1231@gmail.com>
2020-10-27 00:57:45 -0400

    Moved onto main machine, and upgraded Unity version, added first few prefabs/scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Platform")
+            rb2d.velocity = new Vector2(0, 5);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Platform;
+    public GameObject Hazard;
+    public static Spawner instance;
+
+    private List<GameObject> pool;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        
+        pool = new List<GameObject>();
+        PrePool();
+    }
+
+    private void PrePool() {
+        pool.Add(Instantiate(Platform));
+        pool.Add(Instantiate(Platform));
+        pool.Add(Instantiate(Platform));
+        pool.Add(Instantiate(Platform));
+        pool.Add(Instantiate(Hazard));
+        pool.Add(Instantiate(Hazard));
+        pool.Add(Instantiate(Hazard));
+        pool.Add(Instantiate(Hazard));
+    }
+
+    public void Pool(GameObject thing) {
+        Transform trans = thing.GetComponent<Transform>();
+
+        trans.position = new Vector3(-10, -10, 0);
+        thing.SetActive(false);
+
+        pool.Add(thing);
+    }
+
+    public void Spawn(GameObject toSpawn) {
+        GameObject choice = Platform;
+
+        foreach (GameObject go in pool) {
+            if (go.tag == toSpawn.tag) {
+                choice = go;
+                break;
+            }
+        }
+
+        choice.SetActive(true);
+
+        Transform trans = choice.GetComponent<Transform>();
+        trans.position = new Vector3(Random.Range(0, 1080), 1950, 0);
+    }
+}
