3ed0cd747f200f59b9e187f268c30bbfeabf0b5c
Robert Johnson <robertjo1231@gmail.com>
2020-12-18 01:17:10 -0500

    Cleanup in a file

--- Assets/Scripts/BoundingBox.cs
+++ Assets/Scripts/BoundingBox.cs
@@ -6,7 +6,9 @@ public class BoundingBox : MonoBehaviour
 {
 
     private void OnTriggerExit2D(Collider2D other) {
-        Spawner.instance.Pool(other.gameObject);
-        GameManager.instance.LandingCount--;
+        if (other.tag == "Platform" || other.tag == "Hazard") {
+            Spawner.instance.Pool(other.gameObject);
+            GameManager.instance.LandingCount--;
+        }
     }
 }
